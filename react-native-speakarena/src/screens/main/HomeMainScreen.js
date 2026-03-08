ea7b57dfa4adb1cf64e333227ac338549e6120e8
Rishi <ichigoobankai123@gmail.com>
2026-03-08 03:12:42 -0500

    fix: move bottom node 1 onto trail near viewport edge intersection

--- react-native-speakarena/src/screens/main/HomeMainScreen.js
+++ react-native-speakarena/src/screens/main/HomeMainScreen.js
@@ -132,7 +132,7 @@ export default function HomeMainScreen({ navigation }) {
               <View style={[styles.nodePos, { top: 844, left: 96 }]}>
                 <LearningPathNode locked number={5} />
               </View>
-              <View style={[styles.nodePos, { top: 1172, left: 238 }]}>
+              <View style={[styles.nodePos, { top: 1172, left: 258 }]}>
                 <LearningPathNode locked number={1} />
               </View>
             </View>
